0922504b0e8d8a50d1e283c73ce3a16a8e952697
oldcb <37439985+OnlyDescribe@users.noreply.github.com>
2026-05-06 01:32:44 +0800

    fix: support EmmyLua native attach in editor

--- Plugins/slua_unreal/Source/slua_unreal/Private/LuaState.cpp
+++ Plugins/slua_unreal/Source/slua_unreal/Private/LuaState.cpp
@@ -588,7 +588,12 @@ namespace NS_SLUA {
 
         // use custom memory alloc func to profile memory footprint
 #if ENABLE_PROFILER && !UE_BUILD_SHIPPING
+#if WITH_EDITOR
+        // EmmyLua native attach observes luaL_newstate in editor processes.
+        L = luaL_newstate();
+#else
         L = lua_newstate(LuaMemoryProfile::alloc,this);
+#endif
 #else
         L = luaL_newstate();
 #endif
